8911769c43a3f2a1f7f1d405ea66b054cd1ee262
zgr0 <ozkara583@gmail.com>
2024-08-08 07:55:39 +0300

    map and mapb added

--- main.go
+++ main.go
@@ -23,6 +23,12 @@ type Area struct {
 type Data struct {
 	Areas []Area `json:"areas"`
 }
+type config struct {
+	start int
+	end   int
+}
+
+var limit = config{start: 1, end: 5}
 
 func handleInput(cmd cliCommand, ok bool) {
 	if ok {
@@ -61,39 +67,70 @@ func commandHelp(cmdMap map[string]cliCommand) func() error {
 		}
 	}
 */
-func commandList() func() error {
+func commandmap() func() error {
 	return func() error {
-		for i:=1;i<=20;i++{
-		url := fmt.Sprintf("https://pokeapi.co/api/v2/location/%d/?limit=6&offset=0",i)
-		res, err := http.Get(url)
-		if err != nil {
-			log.Fatal(err)
+		if limit.start <= 0 || limit.end < 6 {
+			limit.start = 1
+			limit.end = 5
 		}
-		body, err := io.ReadAll(res.Body)
-		defer res.Body.Close()
-		// Unmarshal JSON data
-		var data Data
-		jsonerr := json.Unmarshal(body, &data)
-		if jsonerr != nil {
-			fmt.Println("Error unmarshalling JSON:", jsonerr)
-			return nil
-		}
+		for i := limit.start; i < limit.end; i++ {
+			if i== 21 {
+				continue
+			}
+			url := fmt.Sprintf("https://pokeapi.co/api/v2/location/%d/?limit=1&offset=0", i)
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatal(err)
+			}
+			body, err := io.ReadAll(res.Body)
+			defer res.Body.Close()
+			// Unmarshal JSON data
+			var data Data
+			jsonerr := json.Unmarshal(body, &data)
+			if jsonerr != nil {
+				fmt.Println("Error unmarshalling JSON:", jsonerr)
+				limit.start = 1
+				limit.end = 5
+				fmt.Printf("i:%d", i)
+				return nil
+			}
+			// Print the name of each area
+			for _, area := range data.Areas {
+				fmt.Println(area.Name)
+			}
 
-		// Print the name of each area
-		for _, area := range data.Areas {
-			fmt.Println(area.Name)
+			if res.StatusCode > 299 {
+				log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-		
-		if res.StatusCode > 299 {
-			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
+		limit.start += 5
+		limit.end += 5
+		return nil
 	}
-	return nil 
+
+}
+func commandMapBack() func() error {
+	return func() error {
+		fmt.Printf("%d\n", limit.start)
+		fmt.Printf("%d\n", limit.end)
+
+		if limit.start <= 0 || limit.end < 6 {
+			fmt.Println("You are at the starting point. You can't go back.")
+			limit.start = 1
+			limit.end = 5
+			return nil
+		} else {
+			limit.start -= 20
+			limit.end -= 20
+			commandFunc := commandmap() // Get the function
+			return commandFunc()        // Call the function
+		}
 	}
 }
+
 func main() {
 	reader := bufio.NewScanner(os.Stdin)
 	cmdMap := make(map[string]cliCommand)
@@ -107,9 +144,13 @@ func main() {
 		name:        "exit",
 		description: "exits from app",
 	}
-	cmdMap["list"] = cliCommand{
-		name:     "list",
-		callback: commandList(),
+	cmdMap["map"] = cliCommand{
+		name:     "map",
+		callback: commandmap(),
+	}
+	cmdMap["mapb"] = cliCommand{
+		name:     "map",
+		callback: commandMapBack(),
 	}
 	for {
 		fmt.Print("pokedex> ")
